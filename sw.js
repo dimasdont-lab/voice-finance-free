842833c59df2ae4d6d4a962496234d75496c9cb2
dimasdont-lab <dimas.don.t@gmail.com>
2026-09-24 04:24:20 +0200

    Redesign adaptive bottom navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='voice-finance-free-v23';
+const CACHE='voice-finance-free-v24';
 const CORE=['./','./index.html','./whisper-worker.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
